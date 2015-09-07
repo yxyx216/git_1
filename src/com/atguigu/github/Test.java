ca8522eef40193c77d45dcae9c76c1702af0187f
yxyx216 <um_z@yahoo.com>
2015-09-08 00:37:28 +0800

    fix bug from github -4

--- src/com/atguigu/github/Test.java
+++ src/com/atguigu/github/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("lz-1");
 		System.out.println("lz-2");
-		system.out.println("github -3");
+		System.out.println("github -3");
 	}
 
 }
